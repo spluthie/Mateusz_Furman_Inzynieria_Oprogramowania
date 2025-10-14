367d58d9db29dc2c6ae1765aa9be33b60708688c
spluthie <spluthiesteam@wp.pl>
2025-10-14 19:36:20 +0200

    Dokonczona klasa main

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -3,8 +3,18 @@ package edu.io;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gold Rush");
+        Board plansza = new Board();
+
+        Token pustePole = new Token("\u30FB");
+        Token worekKasy = new Token("\uD83D\uDCB0");
+        Token gracz = new Token("\uC6C3");
+
+
+        plansza.placeToken(5, 5, gracz);
+        plansza.placeToken(7, 6, worekKasy);
+
+        plansza.square(7, 6);
+        plansza.display();
     }
-
-
 
 }
