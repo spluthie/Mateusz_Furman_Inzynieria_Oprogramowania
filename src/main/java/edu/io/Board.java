2274fd86efdc20bb9d5adbada2a29536efc630d4
spluthie <spluthiesteam@wp.pl>
2025-10-14 19:30:32 +0200

    Zrobiona klasa Board

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -2,31 +2,46 @@ package edu.io;
 
 public class Board {
 
-    public int size;
+    public int size = 8;
     public Token[][] grid;
 
-    public Board(int size) {
-        this.size = size;
+    public Board() {
         this.grid = new Token[size][size];
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                grid[i][j] = new Token("\u30FB");
+            }
+        }
     }
 
     public void clean(){
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                grid[i][j] = null;
+                grid[i][j] = new Token("\u30FB");
             }
         }
     }
 
     public void placeToken(int col, int row, Token token){
-        grid[col][row] = token;
+        grid[row][col] = token;
     }
 
-    public void square(int col, int row){
-        grid = new Token[col][row];
+    public Token square(int col, int row){
+        return grid[row][col];
     }
 
     public void display(){
-
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                Token token = grid[col][row];
+                if (token != null) {
+                    System.out.print(token.label + " ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
