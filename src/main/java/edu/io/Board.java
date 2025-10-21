473c1540ae62b806980cef568de1e43971527109
spluthie <spluthiesteam@wp.pl>
2025-10-21 22:17:17 +0200

    zmodyfikowana klasa Board, dodana metoda randomlyPlaceGold oraz removeGoldAt

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -1,5 +1,7 @@
 package edu.io;
 
+import java.util.Random;
+
 public class Board {
 
     public int size = 8;
@@ -7,12 +9,7 @@ public class Board {
 
     public Board() {
         this.grid = new Token[size][size];
-
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                grid[i][j] = new Token("\u30FB");
-            }
-        }
+        clean();
     }
 
     public void clean(){
@@ -27,6 +24,7 @@ public class Board {
         grid[row][col] = token;
     }
 
+
     public Token square(int col, int row){
         return grid[row][col];
     }
@@ -38,10 +36,25 @@ public class Board {
                 if (token != null) {
                     System.out.print(token.label + " ");
                 } else {
-                    System.out.print(". ");
+                    System.out.print("\u30FB");
                 }
             }
             System.out.println();
         }
     }
+
+    public void randomlyPlaceGold(int amount) {
+        Random random = new Random();
+        for (int i = 0; i < amount; i++) {
+            int x = random.nextInt(size);
+            int y = random.nextInt(size);
+            placeToken(x, y, new Token("\uD83D\uDCB0"));
+        }
+    }
+
+    public void removeGoldAt(int x, int y) {
+        placeToken(x, y, new Token("\u30FB"));
+    }
+
+
 }
