3d7c22314e0d18529639207b20cca7232035437e
spluthie <spluthiesteam@wp.pl>
2025-12-10 01:25:02 +0100

    Echo dziala

--- src/main/java/edu/io/net/Main.java
+++ src/main/java/edu/io/net/Main.java
@@ -1,60 +1,39 @@
 package edu.io.net;
 
-import edu.io.net.command.Command;
 import edu.io.net.command.Echo;
-import edu.io.net.command.Handshake;
-import edu.io.net.command.JoinGame;
-import edu.io.net.command.LeaveGame;
+import edu.io.net.command.Command;
 
-import java.util.Scanner;
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class Main {
-
     public static void main(String[] args) {
+        String host = "localhost";
+        int port = 1313;
+
+        try (Socket socket = new Socket(host, port)) {
 
 
-        String connStr = "tcp://localhost:1313";
+            ObjectOutputStream out =
+                    new ObjectOutputStream(socket.getOutputStream());
 
-        GameServerConnector connector =
-                new GameServerConnector(connStr, new SocketConnector());
-
-
-        connector.onCmdFromServer(cmd -> {
-            System.out.println("[SERVER] -> " + cmd);
-        });
-
-        System.out.println("Connecting to server...");
-        connector.connect()
-                .onSuccess(() -> System.out.println("Connected!"))
-                .onFailure(() -> {
-                    System.out.println("Failed to connect.");
-                    System.exit(1);
-                });
+            ObjectInputStream in =
+                    new ObjectInputStream(socket.getInputStream());
 
 
-        connector.issueCommand(new Handshake.Cmd("1.0"));
+            Echo.Cmd echo = new Echo.Cmd("hello from client");
+            out.writeObject(echo);
+            out.flush();
+
+            System.out.println("Sent: " + echo);
 
 
-        connector.issueCommand(new JoinGame.Cmd("ClientUser"));
+            Command response = (Command) in.readObject();
+            System.out.println("Received: " + response);
 
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type messages to send. 'quit' to exit.");
-
-        while (true) {
-            System.out.print("> ");
-            String line = sc.nextLine().trim();
-
-            if (line.equalsIgnoreCase("quit")) {
-                connector.issueCommand(new LeaveGame.Cmd());
-                connector.disconnect();
-                System.out.println("Disconnected.");
-                break;
-            }
-
-
-            connector.issueCommand(new Echo.Cmd(line));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
